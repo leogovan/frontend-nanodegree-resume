9f9c3e8e16cb768834ae697b813eb8e7112db550
Leofric Govan <leogovan@Leos-MacBook-Pro-2.local>
2016-09-29 21:45:01 +0100

    Commit before deleting a lot of the content

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -20,11 +20,18 @@ var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 $("#main").append([funThoughts]);
 */
 
+// Main header
+
 var name = "Leo Govan";
 var role = "Web Developer";
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
+// bio and contacts
 
 var bio = {
   "name" : "Leo",
@@ -44,12 +51,48 @@ var bio = {
 var formattedPicture = HTMLbioPic.replace("%data%", bio.bioPic);
 var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 var formattedSkills = HTMLskills.replace("%data%", bio.skills);
-var formattedLocation = HTMLlocation.replace("%data%", bio.location);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
 
 $("#header").append(formattedLocation);
 $("#header").append(formattedWelcome);
 $("#header").append(formattedPicture);
 $("#header").append(formattedSkills);
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+// Work
+
+var work = {
+  "jobs" : [{
+    "employer" : "PFSweb",
+    "title" : "Senior Project Manager",
+    "location" : "London",
+    "dates" : "2013-Present",
+    "description" : "Managing complex ecommerce implementations from discovery through to delivery."
+  },{
+    "employer" : "My Family Club",
+    "title" : "Operations Manager",
+    "location" : "London",
+    "dates" : "2010-2013",
+    "description" : "Digital operations including managing the initial implementation and start up, then ongoing operations."
+  }]
+};
+
+work.position = "Project Manager";
+work.employer = "PFS";
+work.years = 3;
+work.city = "Londonshire";
+
+var formattedWorkPosition = HTMLprojectTitle.replace("%data%", work.position);
+
+$("#main").append(formattedWorkPosition);
+
+// Education
+
+var education = {};
+
+education["name"] = "Brunel University";
+education["years"] = "2009";
+education["city"] = "Uxbridge";
+
+var formattedSchoolName = HTMLschoolName.replace("%data%", education.name);
+
+$("#main").append(formattedSchoolName);
